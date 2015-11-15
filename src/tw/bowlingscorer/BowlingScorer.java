6b3b7a95faf817b48e4974b1d7ffda31233ae993
Hanxian Lin <saitoplam@hotmail.com>
2015-11-15 17:00:40 +0800

    return score one  when input one

---
+++ src/tw/bowlingscorer/BowlingScorer.java
@@ -0,0 +1,7 @@
+package tw.bowlingscorer;
+
+public class BowlingScorer {
+    public static int getScoresFromInputNumbers(int i) {
+        return 1;
+    }
+}
--- test/tw/bowlingscorer/BowlingScorerTest.java
+++ test/tw/bowlingscorer/BowlingScorerTest.java
@@ -5,5 +5,10 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class BowlingScorerTest {
+    @Test
+    public void shouldReturnNumberOneWhenInputNumberOne() {
+        assertEquals(1, BowlingScorer.getScoresFromInputNumbers(1));
+    }
+
 
 }
